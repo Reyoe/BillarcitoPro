1e537fe2a2c6b45a19f34d8b655b603017c5ba27
ALDO ULISES MORAN SILVA <Aldo.Moran@alumno.udg.mx>
2020-05-23 16:03:47 -0500

    prueba

--- BillarcitoPro/FormExit.Designer.cs
+++ BillarcitoPro/FormExit.Designer.cs
@@ -41,98 +41,98 @@ namespace BillarcitoPro
 		/// </summary>
 		private void InitializeComponent()
 		{
-			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormExit));
-			this.panelTittle = new System.Windows.Forms.Panel();
-			this.labelTittle = new System.Windows.Forms.Label();
-			this.label1 = new System.Windows.Forms.Label();
-			this.buttonClose = new System.Windows.Forms.Button();
-			this.buttonCancel = new System.Windows.Forms.Button();
-			this.SuspendLayout();
-			// 
-			// panelTittle
-			// 
-			this.panelTittle.BackColor = System.Drawing.Color.Red;
-			this.panelTittle.Dock = System.Windows.Forms.DockStyle.Top;
-			this.panelTittle.Location = new System.Drawing.Point(0, 0);
-			this.panelTittle.Name = "panelTittle";
-			this.panelTittle.Size = new System.Drawing.Size(463, 25);
-			this.panelTittle.TabIndex = 1;
-			// 
-			// labelTittle
-			// 
-			this.labelTittle.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.labelTittle.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-			this.labelTittle.Location = new System.Drawing.Point(102, 28);
-			this.labelTittle.Name = "labelTittle";
-			this.labelTittle.Size = new System.Drawing.Size(251, 23);
-			this.labelTittle.TabIndex = 12;
-			this.labelTittle.Text = "¿Estás seguro de salir?";
-			this.labelTittle.TextAlign = System.Drawing.ContentAlignment.TopCenter;
-			// 
-			// label1
-			// 
-			this.label1.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-			this.label1.Location = new System.Drawing.Point(39, 75);
-			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(391, 72);
-			this.label1.TabIndex = 13;
-			this.label1.Text = "Tienes mesas en juego. Si eliges salir, los procesos\r\nde las mesas no se van a gu" +
-	"ardar. Se recomienda terminar\r\nlos procesos de las mesas en juegos y despues sal" +
-	"ir.\r\n";
-			// 
-			// buttonClose
-			// 
-			this.buttonClose.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
-			this.buttonClose.FlatAppearance.BorderSize = 0;
-			this.buttonClose.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(28)))), ((int)(((byte)(28)))));
-			this.buttonClose.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-			this.buttonClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-			this.buttonClose.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.buttonClose.ForeColor = System.Drawing.SystemColors.ControlDark;
-			this.buttonClose.Location = new System.Drawing.Point(62, 165);
-			this.buttonClose.Name = "buttonClose";
-			this.buttonClose.Size = new System.Drawing.Size(150, 31);
-			this.buttonClose.TabIndex = 1;
-			this.buttonClose.Text = "Salir";
-			this.buttonClose.UseVisualStyleBackColor = false;
-			this.buttonClose.Click += new System.EventHandler(this.ButtonCloseClick);
-			// 
-			// buttonCancel
-			// 
-			this.buttonCancel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
-			this.buttonCancel.FlatAppearance.BorderSize = 0;
-			this.buttonCancel.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(28)))), ((int)(((byte)(28)))));
-			this.buttonCancel.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-			this.buttonCancel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-			this.buttonCancel.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.buttonCancel.ForeColor = System.Drawing.SystemColors.ControlDark;
-			this.buttonCancel.Location = new System.Drawing.Point(250, 165);
-			this.buttonCancel.Name = "buttonCancel";
-			this.buttonCancel.Size = new System.Drawing.Size(150, 31);
-			this.buttonCancel.TabIndex = 2;
-			this.buttonCancel.Text = "Cancelar";
-			this.buttonCancel.UseVisualStyleBackColor = false;
-			this.buttonCancel.Click += new System.EventHandler(this.ButtonCancelClick);
-			// 
-			// FormExit
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(15)))), ((int)(((byte)(15)))));
-			this.ClientSize = new System.Drawing.Size(463, 214);
-			this.Controls.Add(this.buttonCancel);
-			this.Controls.Add(this.buttonClose);
-			this.Controls.Add(this.label1);
-			this.Controls.Add(this.labelTittle);
-			this.Controls.Add(this.panelTittle);
-			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-			this.Name = "FormExit";
-			this.Opacity = 0.85D;
-			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-			this.Text = "FormExit";
-			this.ResumeLayout(false);
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormExit));
+            this.panelTittle = new System.Windows.Forms.Panel();
+            this.labelTittle = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonClose = new System.Windows.Forms.Button();
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // panelTittle
+            // 
+            this.panelTittle.BackColor = System.Drawing.Color.Red;
+            this.panelTittle.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelTittle.Location = new System.Drawing.Point(0, 0);
+            this.panelTittle.Name = "panelTittle";
+            this.panelTittle.Size = new System.Drawing.Size(463, 25);
+            this.panelTittle.TabIndex = 1;
+            // 
+            // labelTittle
+            // 
+            this.labelTittle.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTittle.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.labelTittle.Location = new System.Drawing.Point(102, 28);
+            this.labelTittle.Name = "labelTittle";
+            this.labelTittle.Size = new System.Drawing.Size(251, 23);
+            this.labelTittle.TabIndex = 12;
+            this.labelTittle.Text = "¿Estás seguro de salir?";
+            this.labelTittle.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(39, 75);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(391, 72);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Tienes mesas en juego. Si eliges salir, los procesos\r\nde las mesas no se van a gu" +
+    "ardar. Se recomienda terminar\r\nlos procesos de las mesas en juegos y despues sal" +
+    "ir.\r\n";
+            // 
+            // buttonClose
+            // 
+            this.buttonClose.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.buttonClose.FlatAppearance.BorderSize = 0;
+            this.buttonClose.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(28)))), ((int)(((byte)(28)))));
+            this.buttonClose.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.buttonClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonClose.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonClose.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.buttonClose.Location = new System.Drawing.Point(62, 165);
+            this.buttonClose.Name = "buttonClose";
+            this.buttonClose.Size = new System.Drawing.Size(150, 31);
+            this.buttonClose.TabIndex = 1;
+            this.buttonClose.Text = "Salir";
+            this.buttonClose.UseVisualStyleBackColor = false;
+            this.buttonClose.Click += new System.EventHandler(this.ButtonCloseClick);
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.buttonCancel.FlatAppearance.BorderSize = 0;
+            this.buttonCancel.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(28)))), ((int)(((byte)(28)))));
+            this.buttonCancel.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.buttonCancel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonCancel.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonCancel.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.buttonCancel.Location = new System.Drawing.Point(271, 165);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(150, 31);
+            this.buttonCancel.TabIndex = 2;
+            this.buttonCancel.Text = "Cancelar";
+            this.buttonCancel.UseVisualStyleBackColor = false;
+            this.buttonCancel.Click += new System.EventHandler(this.ButtonCancelClick);
+            // 
+            // FormExit
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(15)))), ((int)(((byte)(15)))));
+            this.ClientSize = new System.Drawing.Size(463, 214);
+            this.Controls.Add(this.buttonCancel);
+            this.Controls.Add(this.buttonClose);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.labelTittle);
+            this.Controls.Add(this.panelTittle);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FormExit";
+            this.Opacity = 0.85D;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FormExit";
+            this.ResumeLayout(false);
 
 		}
 	}
